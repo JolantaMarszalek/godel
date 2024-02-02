4944614476bef08b323171d669e36ad2145c495f
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-02-02 10:59:05 +0100

    Created vizualization

--- godel-project/src/App.tsx
+++ godel-project/src/App.tsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// import ReactAudioAnalyser from "react-audio-analyser";
 import "./App.css";
 
 function App() {
   const [audioFile, setAudioFile] = useState<string | null>(null);
+  const [audioData, setAudioData] = useState<Uint8Array | null>(null);
+
+  useEffect(() => {
+    if (audioFile) {
+      const audioContext = new AudioContext();
+      const audioElement = new Audio(audioFile);
+      const analyserNode = audioContext.createAnalyser();
+      const sourceNode = audioContext.createMediaElementSource(audioElement);
+
+      sourceNode.connect(analyserNode);
+      analyserNode.connect(audioContext.destination);
+
+      const dataArray = new Uint8Array(analyserNode.fftSize);
+
+      const updateDataArray = () => {
+        analyserNode.getByteFrequencyData(dataArray);
+        setAudioData(new Uint8Array(dataArray));
+        requestAnimationFrame(updateDataArray);
+      };
+
+      audioElement.play();
+      updateDataArray();
+    }
+  }, [audioFile]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -14,11 +39,17 @@ function App() {
   return (
     <div className="App">
       <div className="gridContainer">
-        {[...Array(36)].map((_, index) => (
-          <div key={index} className="cell" />
-        ))}
-      </div>{" "}
-      {audioFile && <audio controls src={audioFile} />}
+        {audioData &&
+          Array.from(audioData)
+            .map((value, index) => (
+              <div
+                key={index}
+                className="cell"
+                style={{ height: `${value}px`, backgroundColor: "red" }}
+              />
+            ))
+            .slice(0, 36)}{" "}
+      </div>
       <input type="file" onChange={handleFileChange} />
     </div>
   );
