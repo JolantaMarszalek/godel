a9a37dabbb978a08e0e93183c270fd0b20de0f4d
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2024-02-02 10:29:12 +0100

    Created grid

--- godel-project/src/App.tsx
+++ godel-project/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [audioFile, setAudioFile] = useState<string | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      setAudioFile(URL.createObjectURL(files[0]));
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {audioFile && <audio controls src={audioFile} />}
+      <div className="gridContainer">
+        {[...Array(36)].map((_, index) => (
+          <div key={index} className="cell" />
+        ))}
+      </div>{" "}
+      <input type="file" onChange={handleFileChange} />
+    </div>
+  );
 }
 
-export default App
+export default App;
